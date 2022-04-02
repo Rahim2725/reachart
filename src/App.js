8b4964644ac13c659b77e7b7ebaebfb9b13cbdeb
Rahims609 <rahimboss609@gmail.com>
2022-04-02 20:57:45 +0600

    bar chart added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import MyLineChart from './MyLineChart/MyLineChart';
+import MyLineChart from './componet/MyLineChart/MyLineChart';
+import SpecialChart from './componet/SpecialChart/SpecialChart';
 
 function App() {
   return (
     <div className="App">
       <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
     </div>
   );
 }
